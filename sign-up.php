2c33067229546aea0ef568abae99d9a61ca81bb6
= <=>
2021-01-21 17:07:47 +0530

    server side validation added on sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -1,3 +1,60 @@
+<?php 
+
+    /*signup functionality*/
+    
+    $emailErr = $mobileErr = $passwordErr = "";
+    
+     if($_SERVER['REQUEST_METHOD']=='POST'){
+        $email = sanatize($_POST['email']);
+        $mobile = sanatize($_POST['mobile-no']);
+        $password = sanatize($_POST['passkey']);
+    
+        /*validating and sanitizing data    */
+
+        //validating email
+        if(empty($email) || preg_match('/^\s$/',$email))
+            $emailErr = "This field cannot be empty";
+        else if(!filter_var($email,FILTER_VALIDATE_EMAIL))
+            $emailErr = "Enter a valid email!";
+                
+        //validating mobile no
+        if(empty($mobile) || preg_match('/^\s$/',$mobile))
+            $mobileErr = "This field cannot be empty";
+        else if(!preg_match('/^[0-9]{10}$/',$mobile))
+            $mobileErr = "Enter a valid mobile no.";
+
+        // //validating password
+        if(empty($password) || preg_match('/^\s$/',$password))
+            $passwordErr = "This field cannot be empty";
+        else if(strlen($password)<8 || strlen($password)>15)
+            $passwordErr = "Password must be between 8 to 15 characters.";
+            
+            
+            if($emailErr == "" && $mobileErr == "" && $passwordErr == ""){
+                //every this is valid start here to insert in db
+                //continue from here
+                echo $email, $mobile, $password;
+            }
+
+            else{
+                echo "<script>alert('Please solve below errors!');</script>";
+            }
+    
+
+
+    }
+
+    
+//function for sanitize data
+function sanatize($data){
+    $data = stripslashes($data);
+    $data = trim($data);
+    $data = htmlspecialchars($data);    
+    return $data;
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,14 +78,14 @@
     <!-- form container start -->
     <div class="form-container">
         <h2>Register by filling this form</h2>
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <form action="<?php  htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
         <!-- field-container start -->
         <div class="field-container">
             <label for="email">Email: 
                 <input  type="email" id="email" class="field" name="email" placeholder="example@gmail.com" required />
                 
                 
-                <span class="error"></span>
+                <span class="error"><?php echo $emailErr; ?></span>
             </label>
         </div>
         <!-- field-container end -->
@@ -37,7 +94,7 @@
         <div class="field-container">
             <label for="mobile-no">Mobile No.:
                 <input type="tel" id="mobile-no" class="field" name="mobile-no" placeholder="10 digit" required />
-                <span class="error"></span>
+                <span class="error"><?php echo $mobileErr; ?></span>
             </label>
         </div>
         <!-- field-container end -->
@@ -46,7 +103,7 @@
         <div class="field-container">
             <label for="passkey">Password: 
                 <input type="password" id="passkey" class="field" name="passkey" placeholder="Password" required />
-                <span class="error"></span>
+                <span class="error"><?php echo $passwordErr; ?></span>
             </label>
         </div>
 
